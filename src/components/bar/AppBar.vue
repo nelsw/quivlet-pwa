1bd86261390d7371041b476bc1654154d13db604
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2021-08-16 22:01:30 -0400

    "Quivlet" branding complete; hero container in place, pwa presentable while we switch to service development.

---
+++ src/components/bar/AppBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark
+  >
+    <div
+        class="d-flex align-center"
+    >
+      <v-img
+          contain
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+      />
+      <span
+          class="display-1 ml-2 font-weight-bold"
+          v-text="`Quivlet`"
+      />
+    </div>
+    <v-spacer/>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  namespaced: true
+}
+</script>
---
+++ src/components/container/Hero.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-row
+        class="text-center"
+    >
+      <v-col
+          cols="12"
+      >
+        <v-img
+            contain
+            class="my-3"
+            height="200"
+            :src="require('../../assets/logo.svg')"
+        />
+      </v-col>
+      <v-col
+          class="mb-4"
+      >
+        <h1
+            class="display-2 font-weight-bold mb-3"
+            v-text="`Welcome to Quivlet`"
+        />
+        <p class="subheading font-weight-regular">
+          Broad topic trivia contests;
+          <br>
+          Correct answers advance;
+          <br>
+          Free to play.
+        </p>
+      </v-col>
+      <v-col
+          class="mb-5"
+          cols="12"
+      >
+        <h2
+            class="headline font-weight-bold mb-3"
+            v-text="`... are you game?`"
+        />
+        <v-btn
+            x-large
+            color="primary"
+            v-text="`Let's Play!`"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  namespaced: true,
+}
+</script>
